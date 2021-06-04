23e783f9008911c19c5dd01e1fd4bd30b9bcbf72
Tristan Boyd <trisboyd@gmail.com>
2021-06-03 21:43:28 -0600

    'class created'

---
+++ script/UserInfo.js
@@ -0,0 +1,13 @@
+export default class UserInfo {
+
+    constructor(data) {
+        this._name = data.name;
+        this._descriptor = data.descriptor;
+    }
+
+    getUserInfo() {
+    }
+
+    setUserInfo() {
+    }
+}
